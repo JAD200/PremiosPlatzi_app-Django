25f9387c416ae4875b72a4221ea95d92c27a350c
Juan Agustin Di Pasquo <juandipasquo@gmail.com>
2022-08-30 18:32:37 -0300

    Using URL tag

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     # * ex: /polls/
     path('', views.index, name='index'),
     # ex: /polls/detail/5
-    path("detail/<int:question_id>", views.detail, name="detail"),
+    path('detail/<int:question_id>', views.detail, name='detail'),
     # ex: /polls/results/5
-    path("results/<int:question_id>", views.results, name="results"),
+    path('results/<int:question_id>', views.results, name='results'),
     # ex: /polls/vote/5
-    path("vote/<int:question_id>", views.vote, name="vote"),
+    path('vote/<int:question_id>', views.vote, name='vote'),
 ]
