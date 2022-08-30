1acdce1df5d0f93c79b8d801dae4e42bf061781a
Juan Agustin Di Pasquo <juandipasquo@gmail.com>
2022-08-30 17:29:35 -0300

    Creating views for the app

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse('Hello World')
+    return HttpResponse('Estas en la pagina principal de <h1>Premios Platzi App</h1>')
+
+
+def detail(request, question_id):
+    return HttpResponse(f'Estas viendo la pregunta numero {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'Estas viendo los resultados de la pregunta numero {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'Estas votando a la pregunta numero {question_id}')
