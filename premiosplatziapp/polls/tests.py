75a0b5e612472721a1d8ac8bcc49219f57852b5c
Juan Agustin Di Pasquo <juandipasquo@gmail.com>
2022-09-07 14:55:22 -0300

    Testing views

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,6 +1,8 @@
 import datetime
+from urllib import response
 
 from django.test import TestCase
+from django.urls import reverse
 from django.utils import timezone
 
 from polls.models import Question
@@ -21,7 +23,7 @@ class QuestionModelTests(TestCase):
         self.assertIs(self.question.was_published_recently(), False)
 
     def test_was_published_recently_with_present_questions(self):
-        """test_was_published_recently_with_present_questions returns False for questions which pub_dates are in the present
+        """test_was_published_recently_with_present_questions returns True for questions which pub_dates are in the present
         """
         time = timezone.now() - datetime.timedelta(hours=23)
         self.question.pub_date = time
@@ -36,3 +38,27 @@ class QuestionModelTests(TestCase):
 
 
 # Views tests
+
+class QuestionIndexViewTests(TestCase):
+
+    def test_no_question(self):
+        """test_no_question If no question exist, an appropriate message is displayed
+        """
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available")
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
+
+    def test_questions_with_future_pub_date(self):
+        """test_questions_with_future_pub_date Ensures there is no question with a greater pub_date than the current time
+        """
+        Question(
+            question_text="Present Question", pub_date=timezone.now()).save()
+
+        Question(
+            question_text="Future Question",pub_date=timezone.now() + datetime.timedelta(days=30)).save()
+
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Present Question")
+        self.assertNotContains(response, "Future Question")
