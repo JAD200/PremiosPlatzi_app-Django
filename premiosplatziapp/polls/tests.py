a3de1d32a963f1c133bbed29670b50a858e847f8
Juan Agustin Di Pasquo <juandipasquo@gmail.com>
2022-09-12 14:34:30 -0300

    Improved -- QuestionResultsTests

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from polls.models import Question, Choice
+from polls.models import Question
 
 
 # Models tests
@@ -39,7 +39,7 @@ class QuestionModelTests(TestCase):
 
 
 def create_question(question_text: str, days: int):
-    """create_question create_question Create a question with the given "question text", and published the given 
+    """ create_question Creates a question with the given "question text", and published the given 
     number of days offset to now (negative for past questions, positive for questions to be published)
 
     Args:
@@ -47,18 +47,22 @@ def create_question(question_text: str, days: int):
         days (int): number of the days the question was published (in negative -) or is to be published (in positive +)
     """
     time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text, pub_date=time)
+    question = Question.objects.create(question_text=question_text, pub_date=time)
+    create_choice(question.pk, "Choice1", votes=1)
+    create_choice(question.pk, "Choice2", votes=0)
+    return question
 
 
-def create_choice(choice_text: str, question, votes: int = 0):
-    """create_choice _summary_
+def create_choice(pk, choice_text: str, votes: int = 0):
+    """create_choice Creates a choice with the given "choice_text", the pk it belongs to and the number of votes
 
     Args:
+        pk (pk): choice's primary key
         choice_text (str): text of the choice
-        question (_type_): choice's question
-        votes (int, optional): Votes of the choice. Defaults to 0.
+        votes (int, optional): votes of the choice. Defaults to 0.
     """
-    return Choice.objects.create(choice_text=choice_text, question=question, votes=votes)
+    question = Question.objects.get(pk=pk)
+    return question.choice_set.create(choice_text=choice_text, votes=votes)
 
 
 class QuestionIndexViewTests(TestCase):
@@ -102,10 +106,12 @@ class QuestionIndexViewTests(TestCase):
             response.context["latest_question_list"], [question])
 
     def test_future_and_past_question(self):
-        """test_future_and_past_question Event if both past and future question exist, only past questions are displayed
+        """test_future_and_past_question Even if both past and future question exist, only past questions are displayed
         """
-        past_question = create_question("Past question", days=-30)
-        future_question = create_question("Future question", days=30)
+        past_question = create_question(
+            "Past question", days=-30)
+        future_question = create_question(
+            "Future question", days=30)
 
         response = self.client.get(reverse("polls:index"))
         self.assertQuerysetEqual(
@@ -116,8 +122,10 @@ class QuestionIndexViewTests(TestCase):
     def test_two_past_questions(self):
         """test_two_past_questions The questions index page may display multiple questions
         """
-        past_question1 = create_question("Past question 1", days=-30)
-        past_question2 = create_question("Past question 2", days=-40)
+        past_question1 = create_question(
+            "Past question 1", days=-30)
+        past_question2 = create_question(
+            "Past question 2", days=-40)
 
         response = self.client.get(reverse("polls:index"))
         self.assertQuerysetEqual(
@@ -128,8 +136,10 @@ class QuestionIndexViewTests(TestCase):
     def test_two_future_questions(self):
         """test_two_future_questions The questions index page should display none future questions
         """
-        future_question1 = create_question("Future question 1", days=30)
-        future_question2 = create_question("Future question 2", days=40)
+        future_question1 = create_question(
+            "Future question 1", days=30)
+        future_question2 = create_question(
+            "Future question 2", days=40)
 
         response = self.client.get(reverse("polls:index"))
         self.assertQuerysetEqual(
@@ -143,7 +153,8 @@ class QuestionDetailViewTests(TestCase):
         """test_future_question The detail view of a question with a pub_date in the future
         returns a 404 error not found
         """
-        future_question = create_question("Future question", days=30)
+        future_question = create_question(
+            "Future question", days=30)
         url = reverse("polls:detail", args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
@@ -152,28 +163,54 @@ class QuestionDetailViewTests(TestCase):
         """test_past_question The detail view of a question with a pub_date in the past
         displays the question's text
         """
-        past_question = create_question("Past question", days=-30)
+        past_question = create_question(
+            "Past question", days=-30)
         url = reverse("polls:detail", args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
 
 
 class QuestionResultsViewTests(TestCase):
-    def test_question_not_exists(self):
+    def test_question_not_existence(self):
         """test_question_not_exists If question id does not exists, get 404 error
         """
         response = self.client.get(reverse("polls:results", kwargs={'pk': 1}))
         self.assertEqual(response.status_code, 404)
+
+    def test_with_past_question(self):
+        """test_with_past_question The result view with a pub date in the past display the 
+        question's text
+        """
+        past_question = create_question("past question", days=-15)
+        url = reverse("polls:results", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+    def test_with_future_question(self):
+        """test_with_future_question Questions with a future date aren't displayed and this return a 404 error(not found) 
+        until the date is the specified date
+        """
+        future_question = create_question("this is a future question", days=30)
+        url = reverse("polls:results", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_question_without_choices(self):
+            """test_question_without_choices Questions that have no choices aren't displayed in the index view
+            """
+            time = timezone.now() + datetime.timedelta(days=-1)
+            question = Question.objects.create(question_text="Question test?", pub_date=time)
+            response = self.client.get(reverse("polls:index"))
+            self.assertQuerysetEqual(response.context["latest_question_list"], [])
 
     def test_display_question_choices_and_votes(self):
         """test_display_question_choices_and_votes Page may display votes 
         for every choice of a question
         """
         question = create_question("Question", -1)
-        choice1 = create_choice("Choice 1", question=question, votes=1)
-        choice2 = create_choice("Choice 2", question=question, votes=2)
-        choice3 = create_choice("Choice 3", question=question)
-        choice4 = create_choice("Choice 4", question=question)
+        choice1 = create_choice(question.pk, "Choice 1", votes=1)
+        choice2 = create_choice(question.pk, "Choice 2", votes=2)
+        choice3 = create_choice(question.pk, "Choice 3")
 
         response = self.client.get(
             reverse("polls:results", kwargs={'pk': question.pk}))
@@ -182,4 +219,3 @@ class QuestionResultsViewTests(TestCase):
         self.assertContains(response, choice1.choice_text + ' -- ' + '1 vote')
         self.assertContains(response, choice2.choice_text + ' -- ' + '2 votes')
         self.assertContains(response, choice3.choice_text + ' -- ' + '0 votes')
-        self.assertContains(response, choice4.choice_text + ' -- ' + '0 votes')
