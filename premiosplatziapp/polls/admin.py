8cee9596b58ffdb1300eb42c0e0aa78abb7db08d
Juan Agustin Di Pasquo <juandipasquo@gmail.com>
2022-09-14 14:01:58 -0300

    Improving the Admin -- Change list

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -4,31 +4,34 @@ from django import forms
 from django.forms.models import BaseInlineFormSet
 
 
-class AtLeastOneRequiredInlineFormSet(BaseInlineFormSet):
+class AtLeastTwoRequiredInlineFormSet(BaseInlineFormSet):
 
     def clean(self):
-        """clean Checks that at least one choice has been entered.
+        """clean Checks that at least two choices have been entered.
 
         Raises:
             forms.ValidationError: Shows a message if no choice is entered
         """
-        super(AtLeastOneRequiredInlineFormSet, self).clean()
+        super(AtLeastTwoRequiredInlineFormSet, self).clean()
         if any(self.errors):
             return
         if not any(cleaned_data and not cleaned_data.get('DELETE', False) for cleaned_data in self.cleaned_data):
             raise forms.ValidationError('At least two choices required.')
 
 
-class ChoicesInline(admin.StackedInline):
+class ChoicesInline(admin.TabularInline):
     model = Choice
-    formset = AtLeastOneRequiredInlineFormSet
-    extra = 2
-    exclude = ['votes']
+    formset = AtLeastTwoRequiredInlineFormSet
+    extra = 2 # Number of choices displayed by default
+    exclude = ['votes'] # Field to exclude from the model
 
 
 class QuestionAdmin(admin.ModelAdmin):
     fields = ["pub_date", "question_text"]
     inlines = (ChoicesInline,)
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
     def save_formset(self, request, form, formset, change):
         instances = formset.save(commit=False)
